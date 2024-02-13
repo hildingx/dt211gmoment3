4e2f9a90658a79054c63fa084d5731b441774047
Alexander Hilding <hildingx@gmail.com>
2024-02-13 13:01:15 +0100

    Lagt till klickfunktion på formerna med tillhörande animationer. + Animation på Sass-ikon vid hover.

--- src/js/main.js
+++ src/js/main.js
@@ -20,3 +20,44 @@ function toggleNavMenu() {
 
     icon.classList.toggle("toggle");
 }
+
+let triangleEl = document.querySelector('.triangle');
+let circleEl = document.querySelector('.circle');
+let squareEl = document.querySelector('.square');
+
+triangleEl.addEventListener('click', function() {
+    changeColor(triangleEl);
+});
+
+function changeColor(shape) {
+    shape.classList.add('change-color')
+
+    setTimeout(function() {
+        shape.classList.remove('change-color');
+    }, 2000);
+}
+
+circleEl.addEventListener('click', function() {
+    changeSize(circleEl);
+});
+
+function changeSize(shape) {
+    shape.classList.add('changeSize');
+
+    setTimeout(function() {
+        shape.classList.remove('changeSize');
+    }, 1000); 
+}
+
+squareEl.addEventListener('click', function() {
+    spin(squareEl);
+});
+
+function spin(shape) {
+    shape.classList.add('rotating');
+
+    setTimeout(function() {
+        shape.classList.remove('rotating');
+    }, 2000); 
+}
+
