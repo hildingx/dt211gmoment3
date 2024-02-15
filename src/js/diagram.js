9597b3e33756914eb24c47a4e9526c82cc934420
Alexander Hilding <hildingx@gmail.com>
2024-02-15 15:20:30 +0100

    Skapat ett första diagram med chart.js med input från filterAndSortCourses-funktion. Gjort en workaround gällande responsivitet: Lagt till aspect-ratio: 2 på föräldraelementet för att göra diagrammet responsivt.

--- src/js/diagram.js
+++ src/js/diagram.js
@@ -1,9 +1,15 @@
+import Chart from 'chart.js/auto'
+
 //Funktion för att hämta data från api
 async function fetchData() {
-    const response = await fetch(`https://studenter.miun.se/~mallar/dt211g/`);
-    const data = await response.json();
+    try {
+        const response = await fetch(`https://studenter.miun.se/~mallar/dt211g/`);
+        const data = await response.json();
 
-    return data;
+        return data;
+    } catch (error) {
+        document.getElementById("barChart").innerHTML = `<p>Data kunde inte hämtas</p>`;
+    }
 }
 
 //Funktion som filtrerar ut kurser, sorterar efter sökande och slice'ar ut de 6 första.
@@ -19,18 +25,47 @@ async function filterAndSortCourses() {
     return topCourses;
 }
 
-//Funktion som, just nu, skriver ut kurserna i #barChart.
+//Funktion som skriver ut kurserna i diagram med chart.js
 async function displayTopCourses() {
     let topCourses = await filterAndSortCourses();
-    
-    let barChartEl = document.getElementById('barChart');
-    barChartEl.innerHTML = '';
 
-    topCourses.forEach(course => { 
-        barChartEl.innerHTML += 
-        `
-        <p>${course.name} ${course.applicantsTotal}</p>
-        `
+    // Skapa arrays för kursnamn och antal sökande
+    let courseNames = topCourses.map(course => course.name);
+    let courseApplicants = topCourses.map(course => course.applicantsTotal);
+
+    // Kör Chart.js-diagrammet med kursnamn och antal sökande
+    new Chart(
+        document.getElementById('acquisitions'),
+        {
+        type: 'bar',
+        options: {
+            animation: false,
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+              legend: {
+                display: false
+              },
+              tooltip: {
+                enabled: false
+              }
+            }
+          },
+        data: {
+            labels: courseNames,
+            datasets: [{
+                label: '# of Applicants',
+                data: courseApplicants,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
     });
 }
 
